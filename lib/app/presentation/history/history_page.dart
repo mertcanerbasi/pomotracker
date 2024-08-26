f6a65d7716de1c6145e2465a38d3bdeb594796e7
Mertcan ERBAŞI <mertcanerbasiemk@gmail.com>
2024-08-26 10:12:32 +0300

    search-task-by-name

--- lib/app/presentation/history/history_page.dart
+++ lib/app/presentation/history/history_page.dart
@@ -19,111 +19,282 @@ class _HistoryPageState extends BaseState<HistoryViewModel, HistoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Text("History", style: AppTextStyle.headlineMedium),
-                Spacer(),
-              ],
-            ),
-            StreamBuilder<List<DaysTask?>>(
-              stream: viewModel.tasksStream(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else {
-                  if (snapshot.data?.isEmpty ?? true) {
-                    return ListTile(
+        child: StreamBuilder<List<DaysTask?>>(
+          stream: viewModel.tasksStream(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Column(
+                children: [
+                  Row(
+                    children: [
+                      Text("History", style: AppTextStyle.headlineMedium),
+                      Spacer(),
+                    ],
+                  ),
+                  Center(child: CircularProgressIndicator()),
+                ],
+              );
+            }
+            if (snapshot.hasError) {
+              return Column(
+                children: [
+                  Row(
+                    children: [
+                      Text("History", style: AppTextStyle.headlineMedium),
+                      Spacer(),
+                    ],
+                  ),
+                  Center(child: Text('A problem occurred')),
+                ],
+              );
+            } else {
+              if (snapshot.data?.isEmpty ?? true) {
+                return Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text("History", style: AppTextStyle.headlineMedium),
+                        Spacer(),
+                      ],
+                    ),
+                    ListTile(
                       title: Text("No tasks history"),
                       subtitle: Text(
                           "You have not completed or created any tasks yet"),
                       trailing: Icon(Icons.sentiment_dissatisfied),
-                    );
-                  }
-                  return Expanded(
-                    child: ListView.builder(
-                      itemCount: snapshot.data?.length ?? 0,
-                      itemBuilder: (context, index) {
-                        var reversedData =
-                            snapshot.data?.reversed.toList()[index];
-                        var date = reversedData?.date;
-                        var totalPomodoros = reversedData?.daysTasks.fold(
-                            0, (prev, task) => (prev) + task.pomodoros.length);
-                        var tasks = reversedData?.daysTasks ?? [];
-
-                        return ExpansionTile(
-                          title: Text(
-                            "Date",
-                            style: AppTextStyle.bodyMedium,
-                          ),
-                          subtitle: Text(
-                            date ?? '',
-                          ),
-                          trailing: Text(
-                            "${totalPomodoros ?? 0}",
-                            style: AppTextStyle.bodyMedium,
-                          ),
-                          dense: true,
-                          children: tasks.map((task) {
-                            return ListTile(
-                              leading: task.pomodoros
-                                      .every((pomodoro) => pomodoro.isCompleted)
-                                  ? Icon(
-                                      Icons.check,
-                                      size: 20,
-                                      color: AppColors.accent,
-                                    )
-                                  : Icon(
-                                      Icons.radio_button_unchecked,
-                                      size: 20,
-                                    ),
-                              title: Text(
-                                "${task.name}", // Assuming `task.title` exists
-                              ),
-                              trailing: task.pomodoros
-                                      .every((pomodoro) => pomodoro.isCompleted)
-                                  ? null
-                                  : Container(
-                                      width: 100,
-                                      height: 20,
-                                      clipBehavior: Clip.hardEdge,
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(50),
-                                        color:
-                                            AppColors.accent.withOpacity(0.05),
-                                      ),
-                                      child: Row(
-                                        children: [
-                                          ...task.pomodoros.map((pomodoro) {
-                                            if (pomodoro.isCompleted) {
-                                              return Expanded(
-                                                child: Container(
-                                                  color: AppColors.accent,
-                                                ),
-                                              );
-                                            } else {
-                                              return Spacer();
-                                            }
-                                          }).toList(),
-                                        ],
-                                      )),
-                              subtitle: Text(
-                                "${task.pomodoros.length} pomodoros",
-                              ),
-                            );
-                          }).toList(),
-                        );
-                      },
                     ),
-                  );
-                }
-              },
-            ),
-          ],
+                  ],
+                );
+              } else {
+                return Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text("History", style: AppTextStyle.headlineMedium),
+                        Spacer(),
+                        viewModel.isFiltered
+                            ? IconButton(
+                                onPressed: () {
+                                  viewModel.searchController.clear();
+                                  viewModel.toggleFilter();
+                                },
+                                icon: Icon(Icons.clear),
+                              )
+                            : IconButton(
+                                onPressed: () async {
+                                  viewModel.settasksList(snapshot.data);
+                                  viewModel.toggleFilter();
+                                },
+                                icon: Icon(Icons.search),
+                              ),
+                      ],
+                    ),
+                    AnimatedContainer(
+                      duration: Duration(milliseconds: 300),
+                      height: viewModel.isFiltered ? 80 : 0,
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                        child: Center(
+                          child: TextField(
+                            controller: viewModel.searchController,
+                            onSubmitted: (value) {
+                              viewModel.filterTasks(value);
+                              print(viewModel.filteredTasks);
+                            },
+                            decoration: InputDecoration(
+                              hintText: "Search",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    if (viewModel.isFiltered)
+                      Expanded(
+                        child: Column(
+                          children: [
+                            if (viewModel.filteredTasks?.isEmpty == true ||
+                                viewModel.filteredTasks == null)
+                              ListTile(
+                                title: Text("No tasks"),
+                                subtitle:
+                                    Text("No tasks found for this search"),
+                              ),
+                            Expanded(
+                              child: ListView.builder(
+                                itemCount: viewModel.filteredTasks?.length ?? 0,
+                                itemBuilder: (context, index) {
+                                  var reversedData = viewModel
+                                      .filteredTasks?.reversed
+                                      .toList()[index];
+                                  var date = reversedData?.date;
+                                  var totalPomodoros = reversedData?.daysTasks
+                                      .fold(
+                                          0,
+                                          (prev, task) =>
+                                              (prev) + task.pomodoros.length);
+                                  var tasks = reversedData?.daysTasks ?? [];
+
+                                  return ExpansionTile(
+                                    title: Text(
+                                      "Date",
+                                      style: AppTextStyle.bodyMedium,
+                                    ),
+                                    subtitle: Text(
+                                      date ?? '',
+                                    ),
+                                    trailing: Text(
+                                      "${totalPomodoros ?? 0}",
+                                      style: AppTextStyle.bodyMedium,
+                                    ),
+                                    dense: true,
+                                    children: tasks.map((task) {
+                                      return ListTile(
+                                        leading: task.pomodoros.every(
+                                                (pomodoro) =>
+                                                    pomodoro.isCompleted)
+                                            ? Icon(
+                                                Icons.check,
+                                                size: 20,
+                                                color: AppColors.accent,
+                                              )
+                                            : Icon(
+                                                Icons.radio_button_unchecked,
+                                                size: 20,
+                                              ),
+                                        title: Text(
+                                          "${task.name}", // Assuming `task.title` exists
+                                        ),
+                                        trailing: task.pomodoros.every(
+                                                (pomodoro) =>
+                                                    pomodoro.isCompleted)
+                                            ? null
+                                            : Container(
+                                                width: 100,
+                                                height: 20,
+                                                clipBehavior: Clip.hardEdge,
+                                                decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(50),
+                                                  color: AppColors.accent
+                                                      .withOpacity(0.05),
+                                                ),
+                                                child: Row(
+                                                  children: [
+                                                    ...task.pomodoros
+                                                        .map((pomodoro) {
+                                                      if (pomodoro
+                                                          .isCompleted) {
+                                                        return Expanded(
+                                                          child: Container(
+                                                            color: AppColors
+                                                                .accent,
+                                                          ),
+                                                        );
+                                                      } else {
+                                                        return Spacer();
+                                                      }
+                                                    }).toList(),
+                                                  ],
+                                                )),
+                                        subtitle: Text(
+                                          "${task.pomodoros.length} pomodoros",
+                                        ),
+                                      );
+                                    }).toList(),
+                                  );
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    if (!viewModel.isFiltered)
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: snapshot.data?.length ?? 0,
+                          itemBuilder: (context, index) {
+                            var reversedData =
+                                snapshot.data?.reversed.toList()[index];
+                            var date = reversedData?.date;
+                            var totalPomodoros = reversedData?.daysTasks.fold(0,
+                                (prev, task) => (prev) + task.pomodoros.length);
+                            var tasks = reversedData?.daysTasks ?? [];
+
+                            return ExpansionTile(
+                              title: Text(
+                                "Date",
+                                style: AppTextStyle.bodyMedium,
+                              ),
+                              subtitle: Text(
+                                date ?? '',
+                              ),
+                              trailing: Text(
+                                "${totalPomodoros ?? 0}",
+                                style: AppTextStyle.bodyMedium,
+                              ),
+                              dense: true,
+                              children: tasks.map((task) {
+                                return ListTile(
+                                  leading: task.pomodoros.every(
+                                          (pomodoro) => pomodoro.isCompleted)
+                                      ? Icon(
+                                          Icons.check,
+                                          size: 20,
+                                          color: AppColors.accent,
+                                        )
+                                      : Icon(
+                                          Icons.radio_button_unchecked,
+                                          size: 20,
+                                        ),
+                                  title: Text(
+                                    "${task.name}", // Assuming `task.title` exists
+                                  ),
+                                  trailing: task.pomodoros.every(
+                                          (pomodoro) => pomodoro.isCompleted)
+                                      ? null
+                                      : Container(
+                                          width: 100,
+                                          height: 20,
+                                          clipBehavior: Clip.hardEdge,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(50),
+                                            color: AppColors.accent
+                                                .withOpacity(0.05),
+                                          ),
+                                          child: Row(
+                                            children: [
+                                              ...task.pomodoros.map((pomodoro) {
+                                                if (pomodoro.isCompleted) {
+                                                  return Expanded(
+                                                    child: Container(
+                                                      color: AppColors.accent,
+                                                    ),
+                                                  );
+                                                } else {
+                                                  return Spacer();
+                                                }
+                                              }).toList(),
+                                            ],
+                                          )),
+                                  subtitle: Text(
+                                    "${task.pomodoros.length} pomodoros",
+                                  ),
+                                );
+                              }).toList(),
+                            );
+                          },
+                        ),
+                      ),
+                  ],
+                );
+              }
+            }
+          },
         ),
       ),
     );
--- lib/app/presentation/history/history_page.vm.dart
+++ lib/app/presentation/history/history_page.vm.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:pomotracker/app/model/task.dart';
 import 'package:pomotracker/core/base/base_view_model.dart';
@@ -8,7 +9,48 @@ class HistoryViewModel extends BaseViewModel {
   final LocalDataSource _localDataSource;
   HistoryViewModel(this._localDataSource);
 
+  List<DaysTask?>? _tasksList;
+  List<DaysTask?>? get tasksList => _tasksList;
+  void settasksList(List<DaysTask?>? tasksList) {
+    _tasksList = tasksList;
+    notifyListeners();
+  }
+
+  List<DaysTask?>? _filteredTasks;
+  List<DaysTask?>? get filteredTasks => _filteredTasks;
+  void setfilteredTasks(List<DaysTask?>? filteredTasks) {
+    _filteredTasks = filteredTasks;
+    notifyListeners();
+  }
+
   Stream<List<DaysTask?>> tasksStream() {
     return _localDataSource.tasksStream;
+  }
+
+  bool _isFiltered = false;
+  bool get isFiltered => _isFiltered;
+  void toggleFilter() {
+    if (_isFiltered) {
+      setfilteredTasks(null);
+      searchController.clear();
+    }
+    _isFiltered = !_isFiltered;
+    notifyListeners();
+  }
+
+  TextEditingController searchController = TextEditingController();
+
+  void filterTasks(String value) {
+    if (value.isEmpty) {
+      setfilteredTasks(null);
+      return;
+    }
+    final filtered = tasksList?.where((element) {
+      return element?.daysTasks.any((task) {
+            return task.name.toLowerCase().contains(value.toLowerCase());
+          }) ??
+          false;
+    }).toList();
+    setfilteredTasks(filtered);
   }
 }
